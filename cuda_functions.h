f276ec62e8b99bd3516dfc0c96e96781e3e84f1f
andrewCodeDev <54549221+andrewCodeDev@users.noreply.github.com>
2021-01-18 05:44:18 -0800

    Delete cuda_functions.h

--- cuda_functions.h
+++
@@ -1,8 +0,0 @@
-template <typename F, typename T>
-std::vector<T> transform_vec(F f, const std::vector<T> &X)
-{
-	std::vector<T> Y;
-	Y.reserve(X.size());
-	std::transform(begin(X), end(X), back_inserter(Y), f);
-	return Y;
-}
